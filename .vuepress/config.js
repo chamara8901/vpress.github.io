800069a9bb1b29e83e0151c00a8043f9e2285c23
chamara8901 <chamara@grrow.jp>
2022-07-19 11:11:32 +0900

    config edited

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -14,7 +14,7 @@ module.exports={
                 'frontend',
                 'backend'
             ],
-            '/bolg/':[
+            '/blog/':[
                 '',
             ]
         }
