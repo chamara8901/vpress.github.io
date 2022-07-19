2528b046b6ee638f1cc525860c8cc15f470857d9
chamara8901 <chamara@grrow.jp>
2022-07-19 10:52:52 +0900

    add blog

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,6 +5,7 @@ module.exports={
         nav:[
             {text:'Home',link:'/'},
             {text:'docs',link:'/docs/'},
+            {text:'blog',link:'/blog'},
             {text:'About me',link:'https://github.com'},
         ],
         sidebar:{
