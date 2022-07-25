b219ba0e77e4432fb7d06f0ee03db473a9109d98
chamara8901 <chamara@grrow.jp>
2022-07-25 11:26:52 +0900

    add 12

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -14,7 +14,7 @@ module.exports={
                 'frontend',
                 'backend'
             ],
-            '/blog/':[
+            '/posts/':[
                 '',
             ],
         plugins: [
